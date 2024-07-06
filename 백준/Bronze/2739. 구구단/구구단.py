d2f803d8d5bebe60bc3c54e76165fe3bbd5c10fd
Jeongwoo Choi <l4n14ke4.dev@gmail.com>
2024-07-06 17:00:36 +0900

    [Bronze V] Title: 구구단, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.py
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -1,3 +1,18 @@
-number = int(input())
-for i in range(1, 10): # 1부터 9까지
-    print(f"{number} * {i} = {number * i}") # f-string 사용
+N = int(input())
+for i in range(1, 10): # 시작값 1로 지정
+    print(f"{N} * {i} = {N * i}") # f열 포맷팅
+    
+# 첫번째
+# N = int(input())
+# result = 0
+# i = 1
+# for i in range(9+1):
+#     result = N * i
+#     print(f"{N} * {i} = {result}")
+
+# 두번째
+# N = int(input())
+# result = 0
+# for i in range(1, 10): # 시작값 1로 지정
+#     result = N * i
+#     print(f"{N} * {i} = {result}") # f열 포맷팅
