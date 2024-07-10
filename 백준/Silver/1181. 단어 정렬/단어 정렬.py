a56f6d6ace896c4d32bae96b5c9f4d4f88d44bc8
Jeongwoo Choi <l4n14ke4.dev@gmail.com>
2024-07-10 17:12:52 +0900

    [Silver V] Title: 단어 정렬, Time: 604 ms, Memory: 36552 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,15 +1,15 @@
-# 리스트를 활용한 풀이
-words = []
+# 세트를 이용한 풀이
+words = set() 
 N = int(input())
 
 for _ in range(N):
-    word = input() # 단어들 입력받기
-    if word not in words: # 중복 확인
-        words.append(word) # 중복 없으면 리스트에 추가
+    word = input().strip() 
+    # strip 함수는 여기서는 필요 없음 -> 그냥 씀
+    words.add(word)
 
-# 람다함수로 단어의 길이와 단어를 튜플로 만들고,
-# sort 함수로 튜플로 만든 비교키로 비교 및 정렬
-words.sort(key=lambda x:(len(x), x))
+# 세트를 리스트로 만들어 리스트와 동일하게 람다 함수를 이용해 튜플로 만듦.
+# sorted 함수로 튜플로 만든 비교키로 비교 및 정렬 후 sorted_words라는 새 리스트에 저장
+sorted_words = sorted(list(words), key=lambda x: (len(x), x))
 
-for word in words: # 한줄에 단어 하나씩 출력
+for word in sorted_words:
     print(word)
